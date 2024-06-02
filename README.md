f5fe721ad50b62133310c442bca3a2f46d3f009f
Pablo Castellano <pablo@anche.no>
2024-06-02 13:55:50 +0200

    Swarm batch prometheus exporter

---
+++ README.md
@@ -0,0 +1,23 @@
+# Swarm batch prometheus exporter
+
+This is a simple prometheus exporter to expose Stamp batch metrics for Ethereum Swarm.
+
+## Usage
+
+1. Compile
+
+```
+make build
+```
+
+2. Run
+
+```
+./swarm-exporter
+```
+
+3. Scrape
+
+```
+curl http://localhost:1640
+```
---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+    "encoding/json"
+    "net/http"
+    "log"
+    "math"
+    "time"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type APIResponse struct {
+    Stamps []Stamp `json:"stamps"`
+}
+
+type Stamp struct {
+    BatchID      string `json:"batchID"`
+    Utilization  int    `json:"utilization"`
+    Usable       bool   `json:"usable"`
+    Label        string `json:"label"`
+    Depth        int    `json:"depth"`
+    Amount       string `json:"amount"`
+    BucketDepth  int    `json:"bucketDepth"`
+    BlockNumber  int    `json:"blockNumber"`
+    ImmutableFlag bool  `json:"immutableFlag"`
+    Exists       bool   `json:"exists"`
+    BatchTTL     int    `json:"batchTTL"`
+}
+
+// Declare the metrics
+var (
+    utilizationMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+        Name: "swarm_stamp_batch_utilization",
+        Help: "Stamp batch utilization.",
+    }, []string{"batchID", "label"})
+    ttlMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+        Name: "swarm_stamp_batch_ttl",
+        Help: "Stamp batch TTL.",
+    }, []string{"batchID", "label"})
+    depthMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+        Name: "swarm_stamp_batch_depth",
+        Help: "Stamp batch depth.",
+    }, []string{"batchID", "label"})
+    amountMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+        Name: "swarm_stamp_batch_amount",
+        Help: "Stamp batch amount.",
+    }, []string{"batchID", "label"})
+    capacityMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+        Name: "swarm_stamp_batch_capacity_bytes",
+        Help: "Stamp batch total capacity in bytes.",
+    }, []string{"batchID", "label"})
+    availabilityMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+        Name: "swarm_stamp_batch_available_bytes",
+        Help: "Stamp batch available capacity in bytes.",
+    }, []string{"batchID", "label"})
+    usageMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+        Name: "swarm_stamp_batch_usage_percentage",
+        Help: "Stamp batch usage percentage.",
+    }, []string{"batchID", "label"})
+)
+
+func init() {
+    prometheus.MustRegister(utilizationMetric)
+    prometheus.MustRegister(ttlMetric)
+    prometheus.MustRegister(depthMetric)
+    prometheus.MustRegister(amountMetric)
+    prometheus.MustRegister(capacityMetric)
+    prometheus.MustRegister(availabilityMetric)
+    prometheus.MustRegister(usageMetric)
+}
+
+func GetStampMaximumCapacityBytes(depth int) float64 {
+    return math.Pow(2, float64(depth)) * 4096
+}
+
+func GetStampUsage(utilization, depth, bucketDepth int) float64 {
+  return float64(utilization) / math.Pow(2, float64(depth-bucketDepth))
+
+}
+
+func fetchMetrics() {
+	resp, err := http.Get("http://localhost:1633/stamps")
+    if err != nil {
+        log.Println("Error fetching data:", err)
+        return
+    }
+    defer resp.Body.Close()
+
+    var apiResponse APIResponse
+    if err := json.NewDecoder(resp.Body).Decode(&apiResponse); err != nil {
+        log.Println("Error decoding JSON data:", err)
+        return
+    }
+
+    // Update Prometheus metrics
+    for _, stamp := range apiResponse.Stamps {
+        utilizationMetric.With(prometheus.Labels{"batchID": stamp.BatchID, "label": stamp.Label}).Set(float64(stamp.Utilization))
+        ttlMetric.With(prometheus.Labels{"batchID": stamp.BatchID, "label": stamp.Label}).Set(float64(stamp.BatchTTL))
+        depthMetric.With(prometheus.Labels{"batchID": stamp.BatchID, "label": stamp.Label}).Set(float64(stamp.Depth))
+        //amountMetric.With(prometheus.Labels{"batchID": stamp.BatchID, "label": stamp.Label}).Set(float64(stamp.Amount))
+
+		capacity := GetStampMaximumCapacityBytes(stamp.Depth)
+        capacityMetric.With(prometheus.Labels{"batchID": stamp.BatchID, "label": stamp.Label}).Set(capacity)
+
+		stampUsage := GetStampUsage(stamp.Utilization, stamp.Depth, stamp.BucketDepth)
+        usageMetric.With(prometheus.Labels{"batchID": stamp.BatchID, "label": stamp.Label}).Set(stampUsage)
+
+		available := int(stampUsage * capacity)
+        availabilityMetric.With(prometheus.Labels{"batchID": stamp.BatchID, "label": stamp.Label}).Set(float64(available))
+    }
+}
+
+func main() {
+    // Setup fetchMetrics to run periodically
+    go func() {
+        for {
+            fetchMetrics()
+            time.Sleep(5 * time.Minute) // Adjust fetch interval as needed
+        }
+    }()
+
+    // Setup HTTP server
+    http.Handle("/metrics", promhttp.Handler())
+    log.Fatal(http.ListenAndServe(":1640", nil))
+}
